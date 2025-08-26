c67a3024be93070ba9f61191476b3a8484c734c0
Amanpreet Kaur <amanpreetbhogal66@gmail.com>
2025-08-26 20:10:04 +0530

    Add files via upload

---
+++ math_module.py
@@ -0,0 +1,9 @@
+import math
+num = float(input("Enter a number: "))
+square_root = math.sqrt(num)
+natural_log = math.log(num)
+sine_value = math.sin(num)
+print(f"\nResults for the number: {num}")
+print(f"square root : {square_root}")
+print(f"natural logrithm (base e): {natural_log}")
+print(f"sine (in redians): {sine_value}")
